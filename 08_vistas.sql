99bee62b9d05cc4a68b1235e8689a01741f41549
MariamJava <mariam.java.2596@gmail.com>
2025-05-07 17:01:41 +0200

    s

--- 08_vistas.sql
+++ 08_vistas.sql
@@ -6,3 +6,39 @@ AS
     SELECT  EMP_NO,  APELLIDO, OFICIO, FECHA_ALT, DEPT_NO from EMP;
 
 SELECT * FROM  V_EMPLEADOS;
+-- UNA VISTA SIMPLIFICA LAS CONSULTAS
+--MOSTRAR APPELLIDO, OFICIO, SALARIO, N DEPT, LOC DE EMP
+CREATE OR REPLACE view V_EMP_DEPT
+as 
+    select EMP.APELLIDO, EMP.OFICIO, EMP.SALARIO,
+DEPT.DNOMBRE, DEPT.LOC
+from EMP 
+inner join DEPT 
+on EMP.DEPT_NO = DEPT.DEPT_NO;
+select * from V_EMP_DEPT;
+
+SELECT  * FROM user_views where VIEW_NAME='V_EMP_DEPT';
+select * from V_EMPLEADOS where OFICIO='ANALISTA';
+
+--modificar el salario de los empleados analista 
+select * from V_EMPLEADOS;
+update  EMP set SALARIO = SALARIO +1 where OFICIO='ANALISTA';
+select * from EMP;
+update V_EMPLEADOS set SALARIO = SALARIO  + 1 where OFICIO='ANALISTA';
+
+delete from V_EMPLEADOS where EMP_NO=7917;
+
+insert into V_EMPLEADOS VALUES
+(1111, 'lunes', 'LUNES', 0, sysdate,  40);
+
+create or replace view V_VENDEDORES
+as
+    select  EMP_NO, APELLIDO, OFICIO,
+    SALARIO, DEPT_NO from EMP
+    where OFICIO='VENDEDOR';
+update V_VENDEDORES set SALARIO = SALARIO +1;
+update V_VENDEDORES set OFICIO  = 'VENDIDOS';
+select  * from V_VENDEDORES;
+
+--Vista inutil porque el oficio se ha modificado el where
+rollback;
